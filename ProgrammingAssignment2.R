4b4f98ca2957ab284b9b4871ff7a426adc4496c5
Wenxi493 <2985774094@qq.com>
2020-07-13 10:57:08 +0200

    ProgrammingAssignment2

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()){
+  in_verse<- NULL
+  set <- function(y){
+    x <<- y
+    in_verse <<- NULL
+  }
+  get <- function()x
+  setinverse <- function(inverse) in_verse <<- inverse
+  getinverse <- function()in_verse
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x,...){
+  in_verse <- x$getinverse()
+  if(!is.null(in_verse)){
+    message("getting cached data")
+    return(in_verse)
+  }
+  data <- x$get()
+  in_verse<- solve(data,...)
+  x$setinverse(in_verse)
+  in_verse
+}
